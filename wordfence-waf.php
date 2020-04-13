c0da1714e1b63089ae0cd552b858ac28ddf695ab
okcontabilidad <61439894+okcontabilidad@users.noreply.github.com>
2020-04-12 23:33:45 -0500

    ok17

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('C:\\wamp64\\www\\okcontabilidad\\wp-content\\plugins\\wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", 'C:\\wamp64\\www\\okcontabilidad/wp-content/wflogs/');
+	include_once 'C:\\wamp64\\www\\okcontabilidad\\wp-content\\plugins\\wordfence/waf/bootstrap.php';
+}
+?>
