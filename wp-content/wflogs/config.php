27cc1e74f658c9e7d33e503cf0cb1f3274b920b0
okcontabilidad <61439894+okcontabilidad@users.noreply.github.com>
2020-05-02 16:56:26 -0500

    ok18

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1587353073;s:7:"authKey";s:64:"d ZXp*nig-fms{a3cD(+ABj!$-lp-Rw`fxvQvXi =)8{` -UplRI*{9clO;BwepN";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1026;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"d ZXp*nig-fms{a3cD(+ABj!$-lp-Rw`fxvQvXi =)8{` -UplRI*{9clO;BwepN";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1026;}
