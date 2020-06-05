4579e1ebbe26794531d6dbd9051835e901b958dc
okcontabilidad <61439894+okcontabilidad@users.noreply.github.com>
2020-06-05 10:54:58 -0500

    ok21

--- wp-content/plugins/elementor/assets/js/editor-document.js
+++ wp-content/plugins/elementor/assets/js/editor-document.js
@@ -1,4 +1,4 @@
-/*! elementor - v2.9.10 - 01-06-2020 */
+/*! elementor - v2.9.11 - 02-06-2020 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
